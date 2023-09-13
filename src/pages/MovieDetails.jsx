0567d09b113ea401704c998cf3aad3dac0efb707
Ebiladou <oyinebiladouomemu@gmail.com>
2023-09-13 01:59:52 +0100

    update pages ui

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "../services/BaseApi";
 import Sidebar from "./Sidebar";
+import Rect from '../assets/Rect.png'
 
 function MovieDetails() {
   const { movieId } = useParams();
@@ -23,14 +24,42 @@ function MovieDetails() {
   return (
     <div className="details-container">
       <Sidebar />
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
-        alt={movieDetails.title}
-      />
-      <h1 data-testid = 'movie-title'>{movieDetails.title}</h1>
-      <p data-testid = 'movie-release-date'>{movieDetails.releasedate}</p>
-      <p data-testid = 'movie-runtime'>{movieDetails.runtime} mins</p>
-      <p data-testid = 'movie-overview'>{movieDetails.overview}</p>
+      <div className="movie-details">
+        <div className="image-div">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+            alt={movieDetails.title}
+          />
+        </div>
+
+        <div className="flex-details">
+          <h1 data-testid="movie-title">{movieDetails.title}</h1>
+          <p data-testid="movie-release-date">{movieDetails.releasedate}</p>
+          <p data-testid="movie-runtime">{movieDetails.runtime} mins</p>
+          <p data-testid="movie-genre">{movieDetails.genre}</p>
+        </div>
+        <div className="more-info">
+          <div className="description">
+             <p data-testid="movie-overview">{movieDetails.overview}</p>
+             <p>Directors:</p>
+            <p>Writers:</p>
+            <p>Stars:</p> 
+            <div className="bg-top">
+            <p>Top rated movies #67</p>
+            <p>Awards 9 nominations</p>
+          </div>
+          </div>
+         
+          
+          <div className="image-stack">
+          <p>See Showtime</p>
+          <p>More Watch Options</p>
+          <div className="image-s">
+            <img src={Rect} alt="poster" />
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -9,17 +9,17 @@ import Logout from '../assets/Logout.png'
 function Sidebar() {
   return (
     <div className='sidebar-container'>
-        <img src={Logo} alt="Logo" />
-        <img src={Home} alt="Home icon" /><span>Home</span>
-        <img src={Movie} alt="movie icon" /><span>Movies</span>
-        <img src={TV} alt="tv icon" /><span>TV Series</span>
-        <img src={Calendar} alt="calender icon" /><span>Upcoming</span>
+        <div><img src={Logo} alt="Logo" /></div>
+        <div className='same-div'><img src={Home} alt="Home icon" /><span>Home</span></div>
+        <div className='same-div'><img src={Movie} alt="movie icon" /><span>Movies</span></div>
+        <div className='same-div'><img src={TV} alt="tv icon" /><span>TV Series</span></div>
+        <div className='same-div'><img src={Calendar} alt="calender icon" /><span>Upcoming</span></div>
         <div className="quiz">
             <h3>Play Movie quiezzies and earn free tickets</h3>
             <p>50K people are playing now</p>
             <button>Start Playing</button>
         </div>
-        <img src={Logout} alt="Logout icon" /><span>Logout</span>
+        <div className='same-div'><img src={Logout} alt="Logout icon" /><span>Logout</span></div>
     </div>
   )
 }
